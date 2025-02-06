4424c13a4d04516423ac887dadecd9374e94536c
hamid-mans <h.mansouri2310@gmail.com>
2025-02-06 14:51:25 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ router.post('/cart', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-	res.render('tickethack_front/Index.html')
+	res.render('tickethack_front/Index')
 })
 
 // Supprimer un voyage au panier
